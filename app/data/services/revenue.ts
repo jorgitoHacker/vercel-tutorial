cc2e24db9fcb1ee819a6750cc1a2899b215b452e
jorgitoHacker <126207070+jorgitoHacker@users.noreply.github.com>
2024-09-16 17:20:29 -0400

    Update revenue.ts

--- app/data/services/revenue.ts
+++ app/data/services/revenue.ts
@@ -1,1 +1,13 @@
+import dbConnect from "@/app/lib/dbConnect";
+import Revenue from "../../models/revenue";
 
+export async function getRevenues() {
+  await dbConnect();
+  const res = await Revenue.find({}).lean();
+  return res;
+}
+export async function getCourseById(id: string) {
+  await dbConnect();
+  const res = await Revenue.findById(id).lean();
+  return res;
+}
