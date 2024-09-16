6ffd5fb7e2d5eefe670d92805aba39464e2fd888
jorgitoHacker <126207070+jorgitoHacker@users.noreply.github.com>
2024-09-16 17:25:09 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
+import { getRevenues } from "./data/services/revenue";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     Hola Mundo JAJAJA JOJO
-    </div>
-  );
+  const revenues = await getRevenues();
+  return <div>Hola Mundo JIJIJI {revenues.length}</div>;
 }
