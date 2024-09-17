13be99c0dafc316276eab3aab8b56fb02ed009a5
jorgitoHacker <126207070+jorgitoHacker@users.noreply.github.com>
2024-09-17 15:02:37 -0400

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,13 @@
 // import { Card } from "@/app/ui/dashboard/cards";
 // import RevenueChart from "@/app/ui/dashboard/revenue-chart";
-// import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 
-import { fetchRevenue } from "../lib/data";
+import { fetchRevenue, fetchLatestInvoices } from "../lib/data";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 
 export default async function Page() {
   const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
   return (
     <main>
       <h1 className={`mb-4 text-xl md:text-2xl`}>Dashboard</h1>
@@ -22,7 +23,7 @@ export default async function Page() {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue} />
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
