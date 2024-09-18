23cbafe0d3c03c832e9bf44fd692357631009ef8
jorgitoHacker <126207070+jorgitoHacker@users.noreply.github.com>
2024-09-18 16:49:53 -0400

    Update page.tsx

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -3,8 +3,8 @@ import { CreateInvoice } from "../../ui/invoices/buttons";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import Table from "@/app/ui/invoices/table";
-import { fetchInvoicesPages } from "@/app/lib/data";
-import Pagination from "@/app/ui/invoices/pagination";
+//import { fetchInvoicesPages } from "@/app/lib/data";
+//import Pagination from "@/app/ui/invoices/pagination";
 
 export default async function Page({
   searchParams,
@@ -16,7 +16,7 @@ export default async function Page({
 }) {
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
-  const totalPages = await fetchInvoicesPages(query);
+  //const totalPages = await fetchInvoicesPages(query);
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
