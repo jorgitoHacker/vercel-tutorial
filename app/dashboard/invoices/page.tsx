fb92fd836397389903feb6c24cf02e8ad1259bac
jorgitoHacker <126207070+jorgitoHacker@users.noreply.github.com>
2024-09-18 16:58:56 -0400

    Update page.tsx

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -4,7 +4,7 @@ import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import Table from "@/app/ui/invoices/table";
 import { fetchInvoicesPages } from "@/app/lib/data";
-//import Pagination from "@/app/ui/invoices/pagination";
+import Pagination from "@/app/ui/invoices/pagination";
 
 export default async function Page({
   searchParams,
@@ -19,7 +19,6 @@ export default async function Page({
   const totalPages = await fetchInvoicesPages(query);
   return (
     <div className="w-full">
-      {totalPages}
       <div className="flex w-full items-center justify-between">
         <h1 className={`text-2xl`}>Invoices</h1>
       </div>
@@ -31,7 +30,7 @@ export default async function Page({
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-       
+         <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
