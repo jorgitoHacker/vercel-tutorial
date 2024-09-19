c53b3b61d466525c4e433a6792bd5e2a0c848c20
jorgitoHacker <126207070+jorgitoHacker@users.noreply.github.com>
2024-09-19 11:37:24 -0400

    Update page.tsx

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import EditInvoiceForm from "@/app/ui/invoices/edit-form";
 
-function Edit() {
-  return <div>Edit</div>;
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  const costumersObj = customers.map((customer) => ({
+    id: customer._id.toString(),
+    name: customer.name,
+  }));
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <EditInvoiceForm
+        invoice={{
+          id: invoice._id.toString(),
+          customer_id: invoice.customer_id,
+          amount: invoice.amount,
+          status: invoice.status,
+        }}
+        customers={costumersObj}
+      />
+    </main>
+  );
 }
-
-export default Edit;
