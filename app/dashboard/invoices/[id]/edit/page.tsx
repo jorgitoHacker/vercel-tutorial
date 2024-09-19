30c3fb7ced5d2a50977cc36e20a9670e8f8294da
jorgitoHacker <126207070+jorgitoHacker@users.noreply.github.com>
2024-09-19 11:44:35 -0400

    Update page.tsx

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,13 +1,13 @@
-//import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 //import EditInvoiceForm from "@/app/ui/invoices/edit-form";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
-  //   const [invoice, customers] = await Promise.all([
-  //     fetchInvoiceById(id),
-  //     fetchCustomers(),
-  //   ]);
+  const [invoice, customers] = await Promise.all([
+       fetchInvoiceById(id),
+       fetchCustomers(),
+     ]);
 
   //   const costumersObj = customers.map((customer) => ({
   //     id: customer._id.toString(),
