8f7e0acd4650d395338cbfcc8231c7f5ebe58d1f
jorgitoHacker <126207070+jorgitoHacker@users.noreply.github.com>
2024-09-19 15:29:39 -0400

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,7 +13,8 @@ const FormSchema = z.object({
   status: z.enum(["pending", "paid"]),
   date: z.string(),
 });
-
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
@@ -32,6 +33,26 @@ export async function createInvoice(formData: FormData) {
     status,
     date,
   });
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await dbConnect();
+  await Invoice.findByIdAndUpdate(
+    id,
+    { customer_id: customerId, amount: amountInCents, status },
+    { new: true }
+  );
+
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
