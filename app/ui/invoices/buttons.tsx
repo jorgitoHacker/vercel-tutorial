a264bfc6eadc26b21bee5244bcb8c3d50d2049ec
jorgitoHacker <126207070+jorgitoHacker@users.noreply.github.com>
2024-09-19 09:17:31 -0400

    Update buttons.tsx

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -14,10 +14,9 @@ export function CreateInvoice() {
 }
 
 export function UpdateInvoice({ id }: { id: string }) {
-  console.log(id);
   return (
     <Link
-      href="/dashboard/invoices"
+      href={`/dashboard/invoices/${id}/edit`}
       className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
