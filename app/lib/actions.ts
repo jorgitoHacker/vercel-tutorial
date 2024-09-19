7256a0556a7110271633bfe8245ea8565e22dabf
jorgitoHacker <126207070+jorgitoHacker@users.noreply.github.com>
2024-09-19 16:08:22 -0400

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -59,6 +59,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 
 export async function deleteInvoice(id: string) {
-  await Invoice.deleteOne({ _id: id });
+  await Invoice.findByIdAndDelete(id)
   revalidatePath("/dashboard/invoices");
 }
