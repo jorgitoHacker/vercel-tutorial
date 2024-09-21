f02f949f3361124e9bf53e2941d89b77d69d4d2a
jorgitoHacker <126207070+jorgitoHacker@users.noreply.github.com>
2024-09-21 09:23:56 -0400

    Update page.tsx

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,18 +2,24 @@ import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { TInvoice } from "@/app/lib/definitions";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import EditInvoiceForm from "@/app/ui/invoices/edit-form";
+import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [invoice, customers] = await Promise.all([
-       fetchInvoiceById(id),
-       fetchCustomers(),
-     ]);
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
 
-  const customersObj: {id: string; name: string}[] = customers.map((customer) => ({
-       id: customer._id.toString(),
-       name: customer.name,
-  }));
+  if (!invoice) {
+    notFound();
+  }
+  const customersObj: { id: string; name: string }[] = customers.map(
+    (customer) => ({
+      id: customer._id.toString(),
+      name: customer.name,
+    })
+  );
   const invoiceObj: TInvoice = {
     id: invoice._id.toString(),
     customer_id: invoice.customer_id,
@@ -32,8 +38,7 @@ export default async function Page({ params }: { params: { id: string } }) {
           },
         ]}
       />
-      
-      <EditInvoiceForm customers={customersObj} invoice={invoiceObj} /> 
+      <EditInvoiceForm invoice={invoiceObj} customers={customersObj} />
     </main>
   );
 }
