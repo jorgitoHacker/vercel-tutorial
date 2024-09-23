0682906c2ed037944482ba69921796d7d591f5e0
jorgitoHacker <126207070+jorgitoHacker@users.noreply.github.com>
2024-09-23 14:36:10 -0400

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,8 @@ import Invoice from "../models/invoice";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import dbConnect from "./dbConnect";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 export type State = {
   errors?: {
@@ -116,5 +118,24 @@ export async function deleteInvoice(id: string) {
     return {
       message: "Database Error: Failed to delete invoice.",
     };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
   }
 }
