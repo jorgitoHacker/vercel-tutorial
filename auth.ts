d95dfc1d2f55c3cd83c741b05df0b44d2854eb62
jorgitoHacker <126207070+jorgitoHacker@users.noreply.github.com>
2024-09-24 15:20:42 -0400

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 import { z } from "zod";
 import type { User } from "@/app/lib/definitions";
 import UserModel from "./app/models/user";
@@ -45,6 +46,10 @@ export const { auth, signIn, signOut } = NextAuth({
 
         return null;
       },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 });
